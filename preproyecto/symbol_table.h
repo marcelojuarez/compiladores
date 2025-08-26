319bd9ec41f26f80bd27ac74fc8cbd90e5b8cc5e
juancreynoso <juancreynoso11@gmail.com>
2025-08-26 11:27:49 -0300

    library symbol table

---
+++ preproyecto/symbol_table.h
@@ -0,0 +1,68 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+typedef enum {
+    NODE_NUM,
+    NODE_ID,
+    NODE_OP,
+    NODE_BOOL,
+    NODE  
+} NodeType;
+
+typedef enum{
+    suma,
+    resta,
+    asign,
+    divide,
+    mult,
+    op_or,
+    op_and
+} op;
+
+typedef enum {
+    TYPE_INT,
+    TYPE_BOOL
+} VariableType;
+
+union values {
+    int num;
+    int boolean;
+};
+
+typedef struct infoInt {
+    VariableType type;
+    int value;
+} infoInt;  
+
+typedef struct infoBool {
+    VariableType type;
+    int value;
+} infoBool; 
+
+typedef struct infoId {
+    char* name;
+    VariableType type;
+    union values value;
+} infoId;
+
+typedef struct infoOp {
+    op name;
+    VariableType type;
+    union values value;
+} infoOp;
+
+union type {
+    infoInt NUM;
+    infoBool BOOL;
+    infoId ID;
+    infoOp OP;
+    char* NODE;
+};
+
+typedef struct symbol_table {
+    union type *info;
+    struct symbol_table *next;
+};
+
+
+#endif
