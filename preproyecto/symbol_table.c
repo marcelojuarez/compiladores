92d423a9a81af2f5ffc2f4286b19b3ff8323298a
marcelojuarez <marcelo395juarez@gmail.com>
2025-08-27 08:19:33 -0300

    algunos avances

--- preproyecto/symbol_table.c
+++ preproyecto/symbol_table.c
@@ -2,29 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include "symbol_table.h"
+#include "tree.h"
 
-symbol_table* create_symbol_table_of_tree(symbol_table* tree){
+symbol_table* create_symbol_table_of_tree(node* root){
         symbol_table *table = NULL;
+
+        aux_create_symbol_table_of_tree(root, &table);
 
         return table;
 }
 
-void aux_create_symbol_table_of_tree(symbol_table* tree, symbol_table table) {
-    if (tree == NULL) {
+void aux_create_symbol_table_of_tree(node* root, symbol_table** table) {
+    if (root == NULL) {
         return;
     }
 
-    if (tree->info) {
-
+    if (root->type == NODE_ID) {
+        symbol s; 
+        s.name = root->info->ID.name;
+        s.type = root->info->ID.type;
+        s.value = root->info->ID.value.num;
+        insert_symbol(table, s);
     }
 
-}
-
-symbol create_symbol(char* name, VariableType type, int value){
-    symbol newSymbol;
-    newSymbol.name = name;
-    newSymbol.type = type;
-    newSymbol.value = value;
+    aux_create_symbol_table_of_tree(root->left, table);
+    aux_create_symbol_table_of_tree(root->right, table);
 }
 
 int search_symbol(symbol_table *table, char* name){
@@ -60,7 +62,6 @@ void insert_symbol(symbol_table **table, symbol s){
 }
 
 
-
 void print_symbol_table(symbol_table *table){
     if (table == NULL) {
         printf(" ");
@@ -69,8 +70,9 @@ void print_symbol_table(symbol_table *table){
 
     symbol_table *cursor = table;
     
+    printf("[ ");
+
     while(cursor != NULL){
-        printf("[ ");
         printf("ID:  %s\n", cursor->info.name);
         switch(cursor->info.type) {
             case TYPE_BOOL:
@@ -84,14 +86,37 @@ void print_symbol_table(symbol_table *table){
         if (cursor->info.type == TYPE_BOOL) {
             printf(cursor->info.value == 1 ? "true" : "false");
         } else {
-            printf("VALUE: %d\n", cursor->info.value);
+            printf("VALUE: %d", cursor->info.value);
         }        
-        printf(" ]\n");
+        if (cursor->next != NULL) {
+            printf(" ,\n");
+        }
+
         cursor = cursor->next;
     }
+
+    printf(" ]\n");
 }
 
 int main(){
+    node* nodex = createIdNode("x");
+    nodex->info->ID.type = TYPE_BOOL;
+    printTree(nodex, 0);
 
+    symbol_table* table = create_symbol_table_of_tree(nodex);
+
+    print_symbol_table(table);
+
+    //node* root = createNewTree(nodex, NULL, NULL);
+
+    /*
+    printTree(root, 0);
+
+    printf("\n");
+
+    symbol_table* table = create_symbol_table_of_tree(root);
+
+    print_symbol_table(table);
+    */
     return 0;
 }
--- preproyecto/symbol_table.h
+++ preproyecto/symbol_table.h
@@ -3,6 +3,7 @@
 #define SYMBOL_TABLE
 
 #include "common.h"
+#include "tree.h"
 
 typedef struct symbol {
     char* name;
@@ -15,9 +16,8 @@ typedef struct symbol_table {
     struct symbol_table *next;
 }symbol_table;
 
-symbol_table* create_symbol_table_of_tree(symbol_table* tree);
-void aux_create_symbol_table_of_tree(symbol_table* tree, symbol_table table);
-symbol create_symbol(char* name, VariableType type, int value);
+symbol_table* create_symbol_table_of_tree(node* tree);
+void aux_create_symbol_table_of_tree(node* tree, symbol_table **table);
 int search_symbol(symbol_table *table, char* name);
 void insert_symbol(symbol_table **table, symbol s);
 void print_symbol_table(symbol_table *table);
